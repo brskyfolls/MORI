2c0214c55bdae9c5f7e448a48a9998fc0a0280df
brskyfolls <rodrigo.arismendi.uptp@gmail.com>
2024-10-23 17:06:03 -0300

    First setup

---
+++ app/_layout.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>MORI</Text>
+      <Text style={styles.text}>MORI</Text>
+      <Text style={styles.text}>MORI</Text>
+      <Text style={styles.text}>MORI</Text>
+      <Text style={styles.text}>MORI</Text>
+      <Text style={styles.text}>MORI</Text>
+      <Text style={styles.text}>MORI</Text>
+      <Text style={styles.text}>MORI</Text>
+      <StatusBar style='auto' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center', // Centers the text horizontally
+  },
+});
